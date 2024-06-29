7a6df28f6609990a955d9bc548470410eb3f55d5
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-06-29 22:29:21 +0300

    working with scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const leastFavoriteCustomer='mk'
+var customerName='bob';
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+
+function overwriteBestCustomer(newB){
+  bestCustomer=newB;
+}
+
+function changeLeastFavoriteCustomer(newS){
+    leastFavoriteCustomer=newS;
+}
